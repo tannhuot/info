9ed6916a4fd1ff2da0d853f1a8d0858ca4e94cef
Tannhuot Khouv <huot@Tannhuots-MBP.IT-ACADEMY-STEP>
2024-12-24 17:53:03 +0700

    udpate

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
-const header = document.querySelector(".header.container")
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobile_menu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
 
 document.addEventListener("scroll", () => {
-    let scroll_position = scrollY;
+  let scroll_position = scrollY;
 
-    if (scroll_position > 150) {
-        header.style.backgroundColor = "#29323c";
-    } else {
-        header.style.backgroundColor = "transparent";
-    }
-})
+  if (scroll_position > 150) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
 
-menu_item.forEach(item => {
-    item.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
     mobile_menu.classList.toggle("active");
-    })
-})
+  });
+});
+
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function openttab(tabname) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
